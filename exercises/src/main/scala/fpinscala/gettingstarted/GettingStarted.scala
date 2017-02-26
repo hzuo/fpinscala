094c383c746d25ea0cdaeb7dcea8ab769e503bba
Howard Zuo <someotherusername@gmail.com>
2017-02-26 17:54:03 -0500

    2

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -21,7 +21,7 @@ object MyModule {
     @annotation.tailrec
     def go(n: Int, acc: Int): Int =
       if (n <= 0) acc
-      else go(n-1, n*acc)
+      else go(n - 1, n * acc)
 
     go(n, 1)
   }
@@ -30,7 +30,10 @@ object MyModule {
   def factorial2(n: Int): Int = {
     var acc = 1
     var i = n
-    while (i > 0) { acc *= i; i -= 1 }
+    while (i > 0) {
+      acc *= i;
+      i -= 1
+    }
     acc
   }
 
@@ -38,21 +41,25 @@ object MyModule {
 
   def fib(n: Int): Int = {
     require(n >= 0)
+
     @annotation.tailrec
     def loop(i: Int, a: Int, b: Int): Int = {
       if (i == 0) a
       else loop(i - 1, b, a + b)
     }
+
     loop(n, 0, 1)
   }
 
   def fibAlt(n: Int): Int = {
     require(n >= 0)
+
     @annotation.tailrec
     def loop(i: Int, a: Int, b: Int): Int = {
       if (i == 0) b
       else loop(i - 1, b, a + b)
     }
+
     loop(n, 1, 0)
   }
 
@@ -118,6 +125,7 @@ object TestFib {
 // convenient to have syntax for constructing a function
 // *without* having to give it a name
 object AnonymousFunctions {
+
   import MyModule._
 
   // Some examples of anonymous functions:
@@ -128,7 +136,10 @@ object AnonymousFunctions {
     println(formatResult("increment2", 7, (x) => x + 1))
     println(formatResult("increment3", 7, x => x + 1))
     println(formatResult("increment4", 7, _ + 1))
-    println(formatResult("increment5", 7, x => { val r = x + 1; r }))
+    println(formatResult("increment5", 7, x => {
+      val r = x + 1;
+      r
+    }))
   }
 }
 
@@ -145,13 +156,15 @@ object MonomorphicBinarySearch {
       if (low > high) -mid - 1
       else {
         val mid2 = (low + high) / 2
-        val d = ds(mid2) // We index into an array using the same
-                         // syntax as function application
+        // We index into an array using the same
+        // syntax as function application
+        val d = ds(mid2)
         if (d == key) mid2
-        else if (d > key) go(low, mid2, mid2-1)
+        else if (d > key) go(low, mid2, mid2 - 1)
         else go(mid2 + 1, mid2, high)
       }
     }
+
     go(0, 0, ds.length - 1)
   }
 
@@ -161,7 +174,7 @@ object PolymorphicFunctions {
 
   // Here's a polymorphic version of `binarySearch`, parameterized on
   // a function for testing whether an `A` is greater than another `A`.
-  def binarySearch[A](as: Array[A], key: A, gt: (A,A) => Boolean): Int = {
+  def binarySearch[A](as: Array[A], key: A, gt: (A, A) => Boolean): Int = {
     @annotation.tailrec
     def go(low: Int, mid: Int, high: Int): Int = {
       if (low > high) -mid - 1
@@ -169,36 +182,55 @@ object PolymorphicFunctions {
         val mid2 = (low + high) / 2
         val a = as(mid2)
         val greater = gt(a, key)
-        if (!greater && !gt(key,a)) mid2
-        else if (greater) go(low, mid2, mid2-1)
+        if (!greater && !gt(key, a)) mid2
+        else if (greater) go(low, mid2, mid2 - 1)
         else go(mid2 + 1, mid2, high)
       }
     }
+
     go(0, 0, as.length - 1)
   }
 
   // Exercise 2: Implement a polymorphic function to check whether
   // an `Array[A]` is sorted
-  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = ???
+  def isSorted[A](as: Array[A], gt: (A, A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def loop(i: Int): Boolean = {
+      if (i >= as.length - 1) {
+        true
+      } else {
+        val outOfOrder = gt(as(i), as(i + 1))
+        if (outOfOrder) {
+          false
+        } else {
+          loop(i + 1)
+        }
+      }
+    }
+
+    loop(0)
+  }
 
   // Polymorphic functions are often so constrained by their type
   // that they only have one implementation! Here's an example:
 
-  def partial1[A,B,C](a: A, f: (A,B) => C): B => C =
+  def partial1[A, B, C](a: A, f: (A, B) => C): B => C =
     (b: B) => f(a, b)
 
   // Exercise 3: Implement `curry`.
 
   // Note that `=>` associates to the right, so we could
   // write the return type as `A => B => C`
-  def curry[A,B,C](f: (A, B) => C): A => (B => C) =
-    ???
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = {
+    (a: A) => (b: B) => f(a, b)
+  }
 
   // NB: The `Function2` trait has a `curried` method already
 
   // Exercise 4: Implement `uncurry`
-  def uncurry[A,B,C](f: A => B => C): (A, B) => C =
-    ???
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C = {
+    (a: A, b: B) => f(a)(b)
+  }
 
   /*
   NB: There is a method on the `Function` object in the standard library,
@@ -212,6 +244,8 @@ object PolymorphicFunctions {
 
   // Exercise 5: Implement `compose`
 
-  def compose[A,B,C](f: B => C, g: A => B): A => C =
-    ???
+  def compose[A, B, C](f: B => C, g: A => B): A => C = {
+    (a: A) => f(g(a))
+  }
+
 }
